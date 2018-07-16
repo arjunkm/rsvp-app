137ab9a9585be8116c55715094ff2c9192a9e92e
arjunkm <arjunkoppalmanjunath@gmail.com>
2018-07-16 16:52:38 -0700

    first commit

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AUTH0_DOMAIN: 'rsvp-app.auth0.com', // e.g., kmaida.auth0.com
+  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
+  MONGO_URI: 'mongodb://<dbuser>:<db1308>@ds139331.mlab.com:39331/mean'
+};
